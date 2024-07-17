991475a02be45f0e8a1c1b994110632e6f476624
ramaccari <ramaccari@gmail.com>
2024-07-17 19:03:46 -0300

    Inicio proyecto

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# Título
+
+Spring Boot Demo.
+
+
+## Descripción
+
+Hola mundo desde Spring Boot.
---
+++ src/main/java/com/main/Main.java
@@ -0,0 +1,20 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @RequestMapping("/example")
+    public String hello() {
+        return "Hello desde Spring Boot!";
+    }
+}
